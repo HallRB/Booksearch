2b8634a66de6eaa9b9dfdc2595088aa43d6f1f74
Richard <richardbrianhall@gmail.com>
2021-01-04 15:41:52 -0600

    render search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,23 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import Form from "../components/Form";
-
-import Wrapper from "../Wrapper";
+import Results from "../components/Results";
 import API from "../utils/API";
-
-
-
 
 class Search extends React.Component {
     state = {
         value: "",
-        results: ""
+        books: []
     };
 
+    componentDidMount() {
+        this.searchBook();
+    }
+
+    makeBook = bookData => {
+        return {
+            _id: bookData.id,
+            title: bookData.volumeInfo.title,
+            authors: bookData.volumeInfo.authors,
+            description: bookData.volumeInfo.description,
+            image: bookData.volumeInfo.imageLinks.thumbnail,
+            link: bookData.volumeInfo.previewLink
+        }
+    }
 
     searchBook = query => {
         API.getBook(query)
-            .then(res => this.setState({ results: res.data.data }))
-            .catch(err => console.log(err));
+            .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
+            .catch(err => console.error(err));
     };
 
     handleInputChange = event => {
@@ -33,13 +43,19 @@ class Search extends React.Component {
         this.searchBook(this.state.search);
     };
 
-
     render() {
         return (
-            <Wrapper>
-                <Form />
-                {/* <Results /> */}
-            </Wrapper>
+            <div>
+                <Form
+                    search={this.state.search}
+                    handleInputChange={this.handleInputChange}
+                    handleFormSubmit={this.handleFormSubmit}
+                />
+                <div className="container">
+                    <h2>Results</h2>
+                    <Results books={this.state.books} />
+                </div>
+            </div>
         )
     }
 }
