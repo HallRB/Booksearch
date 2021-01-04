e7db7ebb2682202f7e09c8189041e2726c3560cc
Richard <richardbrianhall@gmail.com>
2021-01-04 15:42:54 -0600

    updated components route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import NavBar from "./NavBar";
-import Header from "./Header";
-import Results from "./Results";
-import Search from "./Search";
-import Wrapper from "./Wrapper";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 import NoMatch from "./pages/NoMatch";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
@@ -12,23 +11,22 @@ import "./App.css";
 
 class App extends Component {
  
-
-    render() {
-      return (
-        <Router>
-          <div>
-            <NavBar />
-            <Header />
-            <Wrapper>
-              <Route exact path="/" Component={Search} />
-              <Route exact path="/search" Component={Search} />
-              <Route exact path="/saved" Component={Saved} />
-              <Route exact path="/noMatch" Component={NoMatch} />
-            </Wrapper>
-          </div>
-        </Router>
-      )
-    };
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
   };
-  
-  export default App;
+};
+
+export default App;
